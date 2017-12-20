b83f6e666cb5cf5b0a4c39aa25bacb1aec7cf694
Jack Reidy <jackreidy@Jacks-MacBook-Pro.local>
2017-12-20 10:49:30 -0500

    all good

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,17 @@
 package quiz;
 
+import java.lang.StringBuilder;
+
 public class ReverseString {
+	//using recursion base case string length <= 2 swap the last two build it up from there
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+       // return new StringBuffer(input).reverse().toString();
+    		String str = "";
+    		if(input.length() <= 1) {
+    			return input;
+    		} else {
+    			str = input.charAt(input.length() - 1) + reverse(input.substring(1, (input.length() - 1))) + input.charAt(0);
+    			return str;
+    		}
     }
 }
